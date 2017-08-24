9414ead0995341a6a48d38dc6b6b1313122d9f49
Leonora Sanchez-Rees <leonora621@yahoo.com>
2017-08-24 16:27:02 -0600

    added post service

--- src/app/components/posts.component.ts
+++ src/app/components/posts.component.ts
@@ -1,9 +1,22 @@
 import {Component, OnInit} from "@angular/core";
 import {ActivatedRoute, Params} from "@angular/router";
 import {Observable} from "rxjs";
+import {PostService} from "../services/post-service";
+import {Post} from "../../../php/classes/post";
 
 @Component({
 	templateUrl: "./templates/posts.php"
 })
 
-export class PostsComponent {}
+export class PostsComponent {
+
+	posts : Post[] = [];
+
+	constructor(protected postService: PostService){}
+
+	getAllPosts() : void {
+		this.postService.getAllPosts()
+			.subscribe(posts=>this.posts = posts);
+
+	}
+}
