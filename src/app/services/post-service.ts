8945992a3721d50e26d9763eeb63d8fe158c72a1
Leonora Sanchez-Rees <leonora621@yahoo.com>
2017-08-24 16:05:42 -0600

    added post service

---
+++ src/app/services/post-service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import {Observable} from "rxjs/Observable";
+import {BaseService} from "./base-service";
+import {Post} from "../../../php/classes/post";
+import {Status} from "../../../php/classes/status";
+
+@Injectable()
+export class PostService extends BaseService {
+	constructor(protected http: Http) {
+		super(http);
+	}
+
+	private postUrl = "apis/post/";
+
+	getAllPosts() : Observable<Post[]> {
+		return(this.http.get(this.postUrl)
+		.map(this.extractData)
+			.catch(this.handleError));
+		}
+
+		getPostbyPostId(postId : number) : Observable<Post> {
+		return(this.http.get(this.postUrl + postId)
+			.map(this.extractData)
+				.catch(this.handleError));
+		}
+
+}
+
